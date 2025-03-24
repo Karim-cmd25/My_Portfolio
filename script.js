ac7c27aa95c9fee84f7ccfea3cfdefcc2d951e19
karim-cmd25 <karimme6900@outlook.fr>
2025-03-24 15:52:19 +0100

    test

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
 document.addEventListener("DOMContentLoaded", function () {
+  console.log("DOM chargé");
+
   // Sélectionnez le formulaire
   const form = document.getElementById("contact-form");
 
   // écouteur d'événements pour l'envoi du formulaire
   form.addEventListener("submit", sendMail);
 
+  console.log("Écouteur d'événements ajouté au formulaire");
+
   // Ajouter l'événement pour fermer la modale lorsque l'on clique en dehors
   const modal = document.getElementById("demo"); // Sélectionne la modale
   const modalContent = modal.querySelector(".modal_content"); // Sélectionne le contenu de la modale
+
+  console.log("Événement de fermeture de la modale configuré");
 
   // Lorsque l'utilisateur clique en dehors de la modale, la fermer
   modal.addEventListener("click", function (event) {
@@ -20,7 +26,10 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 function sendMail(event) {
+  console.log("sendMail appelé");
+
   event.preventDefault(); // Empêche la soumission du formulaire
+  console.log("Formulaire soumis, empêche la soumission par défaut");
 
   // Récupérer les valeurs des champs du formulaire
   let params = {
@@ -29,15 +38,17 @@ function sendMail(event) {
     message: document.getElementById("message").value,
   };
 
+  console.log("Valeurs récupérées du formulaire:", params);
+
   // Envoyer l'email via EmailJS
   emailjs.send("service_an1dy9q", "template_m9dr73k", params).then(
     function (response) {
+      console.log("Réponse de l'email envoyé:", response);
       alert("Mail envoyé avec succès !");
-      console.log("Succès:", response);
     },
     function (error) {
+      console.error("Erreur lors de l'envoi du mail:", error);
       alert("Erreur lors de l'envoi du mail.");
-      console.error("Erreur:", error);
     }
   );
 }
