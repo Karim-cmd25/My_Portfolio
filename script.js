2162d4de2772442df7cc0d9920006110c8829623
karim-cmd25 <karimme6900@outlook.fr>
2025-03-25 15:54:13 +0100

    css

--- script.js
+++ script.js
@@ -8,23 +8,6 @@ document.addEventListener("DOMContentLoaded", function () {
 
   // écouteur d'événements pour l'envoi du formulaire
   form.addEventListener("submit", (e) => sendMail(e));
-
-  console.log("Écouteur d'événements ajouté au formulaire");
-
-  // Ajouter l'événement pour fermer la modale lorsque l'on clique en dehors
-  const modal = document.getElementById("demo"); // Sélectionne la modale
-  const modalContent = modal.querySelector(".modal_content"); // Sélectionne le contenu de la modale
-
-  console.log("Événement de fermeture de la modale configuré");
-
-  // Lorsque l'utilisateur clique en dehors de la modale, la fermer
-  modal.addEventListener("click", function (event) {
-    // Vérifie si le clic s'est produit en dehors du contenu de la modale
-    if (!modalContent.contains(event.target)) {
-      modal.style.visibility = "hidden";
-      modal.style.opacity = "0";
-    }
-  });
 });
 
 function sendMail(event) {
